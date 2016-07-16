ef307756c92d3622d0c1e292db879c77580df158
Hayley McDonald <hhmcdonald@gmail.com>
2016-07-16 13:31:20 -0500

    create custom event listening scripts

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.getElementById('yesBtn').addEventListener('click', function () {
+    this.style.backgroundColor = 'green';
+});
+document.getElementById('noBtn').addEventListener('click', function () {
+    this.style.backgroundColor = 'red';
+});
+
+function sushi(x) {
+    x.style.color = 'blue';
+}
+function icecream(x) {
+    x.style.color = 'red';
+}
+function pizza(x) {
+    x.style.color = 'green';
+}
